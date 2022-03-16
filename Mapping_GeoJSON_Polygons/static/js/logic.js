db6c535369c6a2bc0ffc8e39779ed38b8bd6be6b
ereekaj <52435827+ereekaj@users.noreply.github.com>
2022-03-15 18:00:03 -0600

    mapping polygons

---
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -0,0 +1,50 @@
+// tile layer for streets style.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+//tile layer for dark style map
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets,
+  "Satellite Streets": satelliteStreets
+};
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid', {
+  center: [43.7, -79.3], 
+  zoom: 11, 
+  layers: [satelliteStreets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/ereekaj/Mapping_Earthquakes/main/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
+
+// Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
+    console.log(data);
+// create GeoJSON layer with retrieved data
+L.geoJSON(data).addTo(map):
+});
+
+
+
+// // Creating a GeoJSON layer with the retrieved data.
+//     L.geoJSON(data, {
+//     onEachFeature: function(feature, layer){
+//         console.log(layer);
+//         layer.bindPopup("<h2> Airport Code: " + feature.properties.faa + "</h2> <h3> Airport Name: " + feature.properties.name + "</h3>");
+//     }
+//     }).addTo(map);
+// });
